fbb90180caa7184e50b99fb6f103184f776e4c7d
JUN <gurwns232@naver.com>
2023-09-26 18:22:20 +0900

    search pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     const [pokemons, setPokemons] = useState([]);
     const [offset, setOffset] = useState(0);
     const [limit, setLimit] = useState(20);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         fetchPokeData(true);
@@ -26,9 +27,45 @@ function App() {
         }
     };
 
+    const handelSearchInput = async e => {
+        setSearchTerm(e.target.value);
+        if (e.target.value.length > 0) {
+            try {
+                const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${e.target.value}`);
+                const pokemonData = {
+                    url: `https://pokeapi.co/api/v2/pokemon/${res.data.id}`,
+                    name: searchTerm,
+                };
+                setPokemons([pokemonData]);
+            } catch (error) {
+                setPokemons([]);
+                console.log(error);
+            }
+        } else {
+            fetchPokeData(true);
+        }
+    };
+
     return (
         <article className="pt-6">
-            <header className="flex flex-col gap-2 w-full px-4 z-50"></header>
+            <header className="flex flex-col gap-2 w-full px-4 z-50">
+                <div className="relative z-50">
+                    <form action="" className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto">
+                        <input
+                            type="text"
+                            value={searchTerm}
+                            onChange={handelSearchInput}
+                            className="text-xs w-[20.5rem] h-6 px-2 py-1 bg-[hsl(214,13%,47%)] rounded-lg text-gray-300 text-center"
+                        />
+                        <button
+                            type="submit"
+                            className="text-xs bg-slate-900 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-700"
+                        >
+                            검색
+                        </button>
+                    </form>
+                </div>
+            </header>
             <section className="pt-6 flex flex-col justify-center items-center overflow-auto z-0">
                 <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
                     {pokemons.length > 0 ? (
