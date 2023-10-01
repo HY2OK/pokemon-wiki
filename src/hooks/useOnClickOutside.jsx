f31b0549ac4f10e2dbafe0696466efb06b952bd2
JUN <gurwns232@naver.com>
2023-10-02 00:32:17 +0900

    modal 외부 클릭 custom hooks

---
+++ src/hooks/useOnClickOutside.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+
+export default function useOnClickOutside(ref, handler) {
+    useEffect(() => {
+        const listener = e => {
+            if (!ref.current || ref.current.contains(e.target)) {
+                return;
+            }
+            handler();
+        };
+
+        document.addEventListener('mousedown', listener);
+        return () => {
+            document.addEventListener('mousedown', listener);
+        };
+    }, [ref, handler]);
+}
